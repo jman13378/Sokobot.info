19bc62261865bf9604018d1a99db655d4fa2b676
jman13378 <garconiamail@gmail.com>
2022-10-31 14:36:01 -0400

    Testing jquery

--- js/script4.js
+++ js/script4.js
@@ -1,11 +1,16 @@
 function getDiscordURL() {
-  console.log(getJSONFile(discordurl))
+  console.log(getJSONFile(String("discordurl")));
 }
 
+/**
+ * 
+ * @param {str} v 
+ */
 function getJSONFile(v) {
   $.getJSON("./config.json", function(json) {
-    $.each(json, function(key, value){  
-      if (key== v) {return value}
+    $.each(json, function(key, value) {  
+      if (key == v) {return value} else {console.log(key + ":" + value)}
+      return key + ":" + value
     });
   });
 }
