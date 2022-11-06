52d7a42d461c09f197508b0837ce98508f71821e
jman13378 <garconiamail@gmail.com>
2022-11-06 15:30:41 -0500

    loll

--- js/login.js
+++ js/login.js
@@ -1,11 +1,14 @@
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        // callback
-    });
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
 }
-function onSignIn(googleUser) {
-    profile = googleUser.getBasicProfile();
-    console.log('Email: ' + profile.getEmail());
-    // callback
-}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "687671619687-0rl7re58epguj7hrhi2pqimvegi06ika.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    { theme: "outline", size: "large" } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};
